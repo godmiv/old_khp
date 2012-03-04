405d88ee14c82f2c3ae95dee6dc730e0cef416cb
godmiv <ooo.dig@ya.ru>
2012-03-04 19:12:05 +0400

    Форма засунута в модальное окно, не смог заставить ее появляться сразу, поэтому закоментировал пока.

--- application/views/order/start.php
+++ application/views/order/start.php
@@ -74,15 +74,28 @@ $("#grid").click(function(){
 	//if( gr != null ) jQuery("#grid").jqGrid('editGridRow',gr,{height:280,reloadAfterSubmit:false});
 	//else alert("Please Select Row");
 });
+function opendialog(){
+$("#dialog").dialog({
+	title: "Добавление детали",  	//тайтл, заголовок окна
+	width:550,				//ширина
+	height: 500,			//высота
+	modal: false           	//true -  окно модальное, false - нет
+/*	buttons: {
+		"Добавить текст в окно": function() { $("#dialog").text("опа! текст!"); },
+		"Закрыть": function() { $(this).dialog("close"); }
+	}
+*/
+});
+}
 </script>
-
+<!--input type="button" onclick="opendialog();" value="проба" /-->
+<div id="dialog">
 <?php if (isset($errors)): ?>
 <p>Some errors were encountered, please check the details you entered.</p>
 <?php foreach ($errors as $message): ?>
     <?php echo $message ?><br />
 <?php endforeach ?>
 <?php endif ?>
-
 <?php
 //echo form::open('/order/add');
 echo form::open();
@@ -124,5 +137,6 @@ echo form::select('osin', $opt, '', array('id'=>'osin'));
 	echo form::close();
 	?>
 </table>
+</div>
 <table id="grid"></table>
 <div id="pager"></div>
