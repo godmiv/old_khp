2126694c6ee51634e64a84071da82bcee38dc5a1
godmiv <ooo.dig@ya.ru>
2012-06-29 00:08:47 +0400

    Не помню что изменил :)

--- application/classes/controller/file.php
+++ application/classes/controller/file.php
@@ -9,7 +9,26 @@ class Controller_File extends Controller {
 
 	public function action_upload()
 	{
+		echo '<pre>';
 		print_r($_POST);
 		print_r($_FILES);
+		echo date('Y/m/d');
+		echo '</pre>';
+		$pathname = 'upload/';
+		$sess = Session::instance()->as_array();
+		if(!isset ($sess['user'])){
+			die('Пользователь не залогинен, нужно отсекать это раньше, а не во время записи файла.');
+		} else {
+			$pathname .= date('Y/m/d'). '/' . Arr::get($_POST, 'selectedrow') . '/';
+			$uploadfile = $pathname . basename($_FILES['userfile']['name']);
+			echo $pathname;
+			if(!is_dir($pathname)) mkdir($pathname,0,true);
+			if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+				echo "Файл корректен и был успешно загружен.\n";
+			} else {
+				echo "Файл не сохранен, чтото пошло не так... \n";
+			}
+		}
+
 	}
 }
--- application/views/file/upload.php
+++ application/views/file/upload.php
@@ -1,3 +1,2 @@
 <h1>Загрузка файлов.</h1>
 <?php echo $test;?>
-<?php echo Form::open( null, array( 'enctype' => 'multipart/form-data' ) );?>
